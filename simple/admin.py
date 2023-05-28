b2f9eb04880c5689a10dc0ca5d8ec0b924cb504a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-05-28 19:10:22 +0330

    Admin development for simple
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- simple/admin.py
+++ simple/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post, PostImage, PostVideo
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created', 'short_description', 'author')
+    list_filter = ("status", "author")
+    search_fields = ['title', 'short_description']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post', 'file')
+
+
+@admin.register(PostVideo)
+class PostVideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post', 'file')
