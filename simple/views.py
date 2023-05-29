e0f9ca3b35fe4766f042ed6b0cafa4ce3742723c
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-05-29 18:38:21 +0330

    start work by api for simple
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- simple/views.py
+++ simple/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from simple.serializer import PostSerializer
+
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = PostSerializer
