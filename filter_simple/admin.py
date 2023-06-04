7626fa26d224fd3debdedf3920677ac62696a551
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-04 17:25:30 +0330

    create and completing model , admin
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- filter_simple/admin.py
+++ filter_simple/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from filter_simple.models import Post, PostImage, CategorySimple
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(PostImage)
+admin.site.register(CategorySimple)
--- filter_simple/models.py
+++ filter_simple/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField()
+    description = models.TextField()
+    publish = models.BooleanField(default=False)
+    category = models.ForeignKey('CategorySimple', on_delete=models.PROTECT,
+                                 related_name='post_category')
+
+    def __str__(self):
+        return self.title
+
+
+class PostImage(models.Model):
+    title = models.CharField(max_length=50)
+    file = models.ImageField()
+    post = models.ForeignKey('Post', on_delete=models.PROTECT, related_name='image')
+
+    def __str__(self):
+        return self.title
+
+
+class CategorySimple(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str_(self):
+        return self.title
