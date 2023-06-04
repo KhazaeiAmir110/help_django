86d4e82ae0de4ec8396e868690dabbf0e1594697
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-04 17:34:38 +0330

    Development admin.py
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- filter_simple/admin.py
+++ filter_simple/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from filter_simple.models import Post, PostImage, CategorySimple
 
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(PostImage)
-admin.site.register(CategorySimple)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description', 'publish', 'category')
+    list_filter = ("publish", "category")
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post', 'file')
+
+
+@admin.register(CategorySimple)
+class PostVideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
