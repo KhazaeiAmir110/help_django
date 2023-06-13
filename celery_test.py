0700d8bfa9657277c282403ea0f3647e69e813bd
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-13 09:11:02 +0330

    creat first task
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- celery_test.py
+++ celery_test.py
@@ -1,3 +1,10 @@
 from celery import Celery
+import time
 
 app = Celery('first', broker='amqp://guest@localhost//')
+
+
+@app.task(name='celery_test.adding')
+def add(a, b):
+    time.sleep(10)
+    return a + b
