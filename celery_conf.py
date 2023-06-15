7a65aaeaab6051c066fe83aa8be8974187349ee4
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-15 06:57:49 +0330

    configs serialize https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_serializer https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-accept_content
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- celery_conf.py
+++ celery_conf.py
@@ -1,19 +1,3 @@
-from kombu import Queue, Exchange
-
-default_exchange = Exchange('default', type='direct')
-media_exchange = Exchange('media', type='direct')
-
-task_queues = (
-    Queue('default', default_exchange, routing_key='default'),
-    Queue('video', default_exchange, routing_key='video'),
-    Queue('image', default_exchange, routing_key='image'),
-)
-
-task_default_queue = 'default'
-task_default_exchange = 'default'
-task_default_routing_key = 'default'
-
-task_routes = {
-    'celery_test.add': {'queue': 'video'},
-    'celery_test.sub': {'queue': 'image'},
-}
+task_serializer = 'pickle'
+result_serializer = 'pickle'
+accept_content = ['application/x-python-serialize']
