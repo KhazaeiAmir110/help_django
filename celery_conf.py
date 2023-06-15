0a946aff5ccd5890c3e30d24e2e78510b360d283
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-15 06:34:30 +0330

    Defining queues https://docs.celeryq.dev/en/stable/userguide/routing.html#id2
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- celery_conf.py
+++ celery_conf.py
@@ -1,8 +1,19 @@
-task_default_queue = 'mongard'
+from kombu import Queue, Exchange
+
+default_exchange = Exchange('default', type='direct')
+media_exchange = Exchange('media', type='direct')
+
+task_queues = (
+    Queue('default', default_exchange, routing_key='default'),
+    Queue('video', default_exchange, routing_key='video'),
+    Queue('image', default_exchange, routing_key='image'),
+)
+
+task_default_queue = 'default'
+task_default_exchange = 'default'
+task_default_routing_key = 'default'
 
 task_routes = {
-    'celery_test.add': {'queue': 'first'},
-    'celery_test.sub': {'queue': 'second'},
+    'celery_test.add': {'queue': 'video'},
+    'celery_test.sub': {'queue': 'image'},
 }
-
-# celery  -A celery_test worker -l info -Q first,second
