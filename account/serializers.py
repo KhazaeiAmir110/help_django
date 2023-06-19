b3b845a3d4c4c14192e7aa98b28b3e8be0d69346
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-19 17:55:23 +0330

    کاربر باید موارد را وارد کند
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- account/serializers.py
+++ account/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 
 class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    password = serializers.CharField()
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
