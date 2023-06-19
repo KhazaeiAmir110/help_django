648a5cb942a381ca4eedc107ec6622243aa09d24
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-19 17:48:26 +0330

    start app accounts
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- help_django/urls.py
+++ help_django/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('home/', include('home.urls'))
+    path('home/', include('home.urls')),
+    path('accounts/', include('account.urls'), namespaces='accounts'),
 ]
