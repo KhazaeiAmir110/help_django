4cc82271c3dce09a1fc2e080249476deb5d17069
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-06-21 19:25:59 +0330

    methods Questions GET , POST , PUT , DELETE
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import Home
+from .views import Home, QuestionView
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('questions/', QuestionView.as_view(), name='question'),
+    path('questions/<int:pk>/', QuestionView.as_view()),
 ]
+
+# چون برای put نیاز به pk است به همین دلیل باید یک url جدید برای آن تعریف کنیم
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Person
-from .serializers import PersonSerializer
+from .models import Person, Question, Answer
+from .serializers import PersonSerializer, QuestionSerializer, AnswerSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 
 class Home(APIView):
@@ -12,3 +13,31 @@ class Home(APIView):
         persons = Person.objects.all()
         ser_data = PersonSerializer(instance=persons, many=True)
         return Response(data=ser_data.data)
+
+
+class QuestionView(APIView):
+    def get(self, request):
+        question = Question.objects.all()
+        ser_data = QuestionSerializer(instance=question, many=True)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = QuestionSerializer(data=request.data)
+        if data.is_valid():
+            data.save()
+            return Response(data.data, status=status.HTTP_201_CREATED)
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        ser_data = QuestionSerializer(instance=question, data=request.data, partial=True)
+        # partial : برای این است که ما فقط بخواهیم یکی از موار سوال را تغییر دهیم نه تمام موارد آن را
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_200_OK)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        question.delete()
+        return Response({'massage': 'questions delete'}, status=status.HTTP_200_OK)
