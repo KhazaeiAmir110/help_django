405129821af082d5aeb0db60a6ecf100db314a82
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-17 10:15:57 +0330

    working with Throttling https://www.django-rest-framework.org/api-guide/throttling/#throttling --------------------------- The noteworthy point is that this item is not used in security.
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- help_django/settings.py
+++ help_django/settings.py
@@ -137,4 +137,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute',
+        'user': '3/minute',
+    }
 }
