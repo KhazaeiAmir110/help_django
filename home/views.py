c51170b30e674e258273f8b6901c7651cd8a65e9
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-18 11:15:19 +0330

    Method options in View
    https://docs.djangoproject.com/en/4.2/ref/class-based-views/base/#django.views.generic.base.View.options
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ from django.views import View
 class Home(View):
     def get(self, request):
         return render(request, 'home/home.html')
+
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers['host'] = 'localhost'
+        response.headers['user'] = request.user
+        return response
