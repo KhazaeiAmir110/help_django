78faa31c91c3604ff976395c51299804261df4d4
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-19 17:25:34 +0330

    .
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -5,14 +5,5 @@ from django.views import View
 # Create your views here.
 
 class Home(View):
-    def post(self, request):
+    def get(self, request):
         return render(request, 'home/home.html')
-
-    def options(self, request, *args, **kwargs):
-        response = super().options(request, *args, **kwargs)
-        response.headers['host'] = 'localhost'
-        response.headers['user'] = request.user
-        return response
-
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        return render(request=request, template_name='home/method_not_alloewrd.html')
