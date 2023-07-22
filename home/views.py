9b14ce688d13866ae6a012e82f004beead6cac35
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-22 07:36:02 +0330

    work with RedirectView¶
    https://docs.djangoproject.com/en/4.2/ref/class-based-views/base/#redirectview
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .models import Car
 
 
@@ -12,3 +12,23 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         context['cars'] = Car.objects.all()
         return context
+
+
+class Two(RedirectView):
+    # # برای انتقال به سایت های دیگر
+    # url = 'https://www.django-rest-framework.org/api-guide/pagination/'
+
+    # برای انتقال به خود صفحه های سایت
+    pattern_name = 'home:home'
+
+    # # برای کارهای seo البته کار همون pattern_name زو انجام میده
+    # permanent = 'home:home'
+
+    # اگر مقدار دهی اضافی به url بدهیم هیچ ارروری رخ نمیدهد
+    query_string = True
+
+    # نمایش در terminal
+    def get_redirect_url(self, *args, **kwargs):
+        print('*' * 90)
+        print('print')
+        return super().get_redirect_url(*args, **kwargs)
