b6868ef617ca8594cba7d705eebeecd3ed9fcf71
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-22 07:57:44 +0330

    work with listview
    https://docs.djangoproject.com/en/4.2/ref/class-based-views/generic-display/#listview
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -18,3 +18,13 @@ class Home(ListView):
     template_name = 'home/home.html'
     queryset = Car.objects.filter(year__gte=2000)
     context_object_name = 'cars'
+
+    # برای اعمال بیشتر باید در تابع بنویسیم
+    def get_queryset(self):
+        result = Car.objects.filter(year__gte=2000)
+        return result
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['username'] = 'jack'
+        return context
