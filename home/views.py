a11d5fbc51ae6cd6e3f3bbd741b0830cf6200aa1
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-22 07:47:15 +0330

    فیلتر کردن براساس فیلد دلخواه filter Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -16,9 +16,5 @@ from django.views.generic.list import ListView
 
 class Home(ListView):
     template_name = 'home/home.html'
-    model = Car  # object_list
-    # یا باید در html cars را عوض کنیم و object_list بنویسیم یا متد زیر رو بنویسیم
+    queryset = Car.objects.filter(year__gte=2000)
     context_object_name = 'cars'
-
-    # ترتیب
-    ordering = 'year'
