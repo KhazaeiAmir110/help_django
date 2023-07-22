045ecd46c88ba9cb8470e99a6082813f8788d311
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-07-22 06:58:19 +0330

    creat model car
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
