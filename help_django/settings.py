34a52d981b7c8a3144314d0afe0d5c0ac2fdf575
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-02 17:54:14 +0330

    Solve the error
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- help_django/settings.py
+++ help_django/settings.py
@@ -119,7 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -129,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# مدلی که تعریف کردیم خود جنگو بشناسد.
+AUTH_USER_MODEL = 'accounts.User'
