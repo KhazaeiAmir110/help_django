cb99bc99537b8c94db12b5766634d91c880fef3f
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-02 17:48:32 +0330

    Change admin
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phon_number', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'phon_number', 'full_name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('email', 'phon_number', 'full_name', 'password1', 'password2')}),
+    )
+
+    search_fields = ('email', 'full_name')
+    ordering = ('full_name',)
+    # برای permisein ها است چون نداریم خالی است
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group)
