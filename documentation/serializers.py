59e6ee1dae972f394f40bd39b32f058376615061
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-14 08:17:50 +0330

    api and serializer documentation
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

---
+++ documentation/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Documentation
+
+
+class DocumentationSerializer(serializers.ModelSerializer):
+    type_display = serializers.SerializerMethodField()
+    status_display = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Documentation
+        fields = (
+            'id', 'name', 'author', 'type_display', 'status_display'
+        )
+
+    def get_type_display(self, obj):
+        return obj.get_type_display()
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
--- documentation/views.py
+++ documentation/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from .models import Documentation
+from .serializers import DocumentationSerializer
 
-# Create your views here.
+
+class DocumentationViewSet(mixins.ListModelMixin, GenericViewSet):
+    queryset = Documentation.objects.all()
+    serializer_class = DocumentationSerializer
+
+
+class DocumentationBackOfficeViewSet(mixins.ListModelMixin,
+                                     mixins.CreateModelMixin,
+                                     mixins.RetrieveModelMixin,
+                                     mixins.UpdateModelMixin,
+                                     mixins.DestroyModelMixin,
+                                     GenericViewSet):
+    queryset = Documentation.objects.all()
+    serializer_class = DocumentationSerializer
+    lookup_field = 'id'
