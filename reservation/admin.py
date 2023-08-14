9a2a63295df6d131d6877954e31d7c7e6ad5e141
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-14 08:34:30 +0330

    create model and admin reservation
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'documentation', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    search_fields = ['user__username', 'documentation__title']
+    date_hierarchy = 'created'
+
+
+admin.site.register(Reservation, ReservationAdmin)
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
+from documentation.models import Documentation
 
-# Create your models here.
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    documentation = models.ForeignKey(Documentation, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
