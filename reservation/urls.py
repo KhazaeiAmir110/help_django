d1652192d0198da8e44e16bbebf7a461d680823f
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-08-17 11:36:36 +0330

    creat api Reservation https://paper.dropbox.com/#:uid=882287066645500746746892&h2=%D8%AC-%D8%B1%D8%B2%D8%B1%D9%88-%DA%A9%D8%AA%D8%A7%D8%A8:
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import (
-    ReservationViewSet, ReservationListViewSet, UserReservationListAPIView,
-)
+from .views import ReservationViewSet
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'create', ReservationViewSet)
-router.register(r'list', ReservationListViewSet)
+router.register(r'list', ReservationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('list-user-reservation/', UserReservationListAPIView.as_view())
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -8,41 +8,21 @@ from .serializers import (
     ReservationCreateOutputSerializer, ReservationListSerializer, ReservationCreateInputSerializer
 )
 from rest_framework.permissions import IsAuthenticated
+from user.models import User
+from documentation.models import Documentation
 
 
-class ReservationViewSet(mixins.CreateModelMixin,
+class ReservationViewSet(mixins.ListModelMixin,
+                         mixins.RetrieveModelMixin,
+                         mixins.CreateModelMixin,
                          GenericViewSet):
-    queryset = Reservation.objects.filter()
-    serializer_class = ReservationCreateOutputSerializer
-    lookup_field = 'id'
-    permission_classes = (IsAuthenticated,)
-
-    def create(self, request, *args, **kwargs):
-        serializer = ReservationCreateInputSerializer(
-            data=dict(
-                user=request.user.id,
-                documentation=request.data.get('documentation')
-            )
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-
-        reservation_serializer = ReservationCreateOutputSerializer(serializer.instance)
-
-        headers = self.get_success_headers(reservation_serializer.data)
-        return Response(reservation_serializer.data, status=status.HTTP_201_CREATED,
-                        headers=headers)
-
-
-class ReservationListViewSet(mixins.ListModelMixin,
-                             mixins.RetrieveModelMixin,
-                             GenericViewSet):
     queryset = Reservation.objects.all()
-    serializer_class = ReservationListSerializer
+    serializer_class = ReservationCreateOutputSerializer
     lookup_field = 'id'
     filter_backends = [
         filters.SearchFilter, DjangoFilterBackend, filters.OrderingFilter
     ]
+    permission_classes = (IsAuthenticated,)
     search_fields = ['user_name', ]
     filterset_fields = ['user', ]
     ordering_fields = 'created'
@@ -54,11 +34,43 @@ class ReservationListViewSet(mixins.ListModelMixin,
 
     pagination_class = CustomPagination
 
-
-class UserReservationListAPIView(generics.ListAPIView):
-    serializer_class = ReservationListSerializer
-    permission_classes = [IsAuthenticated]
-
     def get_queryset(self):
-        user = self.request.user
-        return Reservation.objects.filter(user=user)
+        queryset = super().get_queryset()
+        if self.action == 'list':
+            return queryset.filter(user=self.request.user)
+
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        documentation = Documentation.objects.get(id=request.POST.get('documentation'))
+        if (request.user.status == 'N' and str(
+                documentation.status) == 'A') or request.user.status == 'S':
+            from test_reserv.tasks import add_payment
+            add_payment()
+            serializer = ReservationCreateInputSerializer(
+                data=dict(
+                    user=request.user.id,
+                    documentation=request.data.get('documentation')
+                )
+            )
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+
+            reservation_serializer = ReservationCreateOutputSerializer(serializer.instance)
+
+            headers = self.get_success_headers(reservation_serializer.data)
+            documentation.status = 'U'
+            return Response(reservation_serializer.data, status=status.HTTP_201_CREATED,
+                            headers=headers)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        from test_reserv.tasks import add_payment
+        add_payment()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = ReservationListSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = ReservationListSerializer(queryset, many=True)
+        return Response(serializer.data)
