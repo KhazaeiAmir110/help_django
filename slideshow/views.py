1d6d11e2cae164942f52c741ebedf9d6653a5069
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-09-28 20:26:05 +0330

    crate view
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- slideshow/views.py
+++ slideshow/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Slideshow
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'slideshow.html')
+    context = {
+        'slides': Slideshow.objects.filter(status=True, post__status=True)
+    }
+    return render(request, 'slideshow.html', context)
