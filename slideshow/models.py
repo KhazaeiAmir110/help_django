e818e2c3d28ae7fd365786110bfb8313beef0cc8
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-09-28 20:08:02 +0330

    create model post and slideshow
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- slideshow/models.py
+++ slideshow/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(default=False)
+
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
+
+
+class Slideshow(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-post__publish']
+
+    def __str__(self):
+        return self.post.title
