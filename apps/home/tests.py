622e1f3a3af9fe154535068fc07152b26ea7e71e
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-10-21 09:57:45 +0330

    package test
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- apps/home/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-
-
-class TestFirst(SimpleTestCase):
-    def test_fals(self):
-        self.assertFalse(1 == 2)
-
-    def test_true(self):
-        self.assertTrue(1 == 1)
