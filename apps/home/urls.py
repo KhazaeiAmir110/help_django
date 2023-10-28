7b23cd4b40648a6f7d8dc43c13c89f75809f8600
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-10-28 08:37:41 +0330

    create form for register
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, AboutView
+from .views import HomeView, AboutView, UserRegisterView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/<str:username>/', AboutView.as_view(), name='about'),
+    path('register/', UserRegisterView.as_view(), name='user_register'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+from .forms import UserRegistrationForm
 
 
-# Create your views here.
 class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
@@ -11,3 +14,26 @@ class HomeView(View):
 class AboutView(View):
     def get(self, request, username):
         return render(request, 'home.html')
+
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super.dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
+            messages.success(request, 'your registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
