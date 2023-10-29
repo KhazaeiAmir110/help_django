0389e901d890f227297e432068714995c0049a02
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-10-29 20:01:54 +0330

    update function post for UserRegister
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import random
+from django.contrib import messages
+
 from django.views import View
 from .forms import UserRegisterForm
+from cor.utils import send_top_code
+from .models import OtpCode
 
 
 class UserRegisterView(View):
@@ -11,4 +16,21 @@ class UserRegisterView(View):
         return render(request, 'accounts/register.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            random_code = random.randint(1000, 9999)
+            # فرستادن کد و شماره تلفن به سرویس برای ارسال به کاربر
+            send_top_code(form.changed_data['phone'], code=random_code)
+            # save phone and code in database
+            OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
+            # save Information in session
+            request.session['user_register_info'] = {
+                'phone_number': form.cleaned_data['phone'],
+                'email ': form.cleaned_data['email'],
+                'full_name': form.cleaned_data['full_name'],
+                'password': form.cleaned_data['password'],
+            }
+            messages.success(request, 'we sent you a code', 'success')
+            # if success sent code => new page
+            return redirect('accounts:verify_code')
+        return redirect('home:home')
