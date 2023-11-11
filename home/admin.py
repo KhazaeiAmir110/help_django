d072eb60e475fdfa365e81735bee26900e51c750
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-11-11 19:08:55 +0330

    create admin post
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Post, Category, File
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'status',
+        'picture'
+    )
+    list_display_links = ('title',)
+    search_fields = ('title', 'description')
+    ordering = ['created']
+
+    def category_to_str(self, obj):
+        return ", ".join([category.title for category in obj.category.all()])
+
+
+class FiledAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'file',
+        'post'
+    )
+    search_fields = ('title',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'published'
+    )
+    list_filter = ('published',)
+    search_fields = ('title',)
+    ordering = ['published']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(File, FiledAdmin)
+admin.site.register(Category, CategoryAdmin)
