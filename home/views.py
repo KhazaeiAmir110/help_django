9c822f9e34ab55b664a8e8c56d485b20eacddb9b
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2023-11-12 07:57:03 +0330

    create view posts
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class ListPostView(ListView):
+    model = Post
+    template_name = "index.html"
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return super(ListPostView, self).get_queryset().filter(status=Post.StatusEnum.PUBLISHED)
