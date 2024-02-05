691fa3e93034b001da57e044ce71509bbf9adf57
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-05 19:32:37 +0330

    save
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- accounts/views.py
+++ accounts/views.py
@@ -18,12 +18,9 @@ class UserRegisterView(View):
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
-            random_code = random.randint(1000, 9999)
-            # فرستادن کد و شماره تلفن به سرویس برای ارسال به کاربر
+            random_code = random.randint(10000, 99999)
             send_top_code(form.changed_data['phone'], code=random_code)
-            # save phone and code in database
             OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
-            # save Information in session
             request.session['user_register_info'] = {
                 'phone_number': form.cleaned_data['phone'],
                 'email ': form.cleaned_data['email'],
@@ -31,7 +28,6 @@ class UserRegisterView(View):
                 'password': form.cleaned_data['password'],
             }
             messages.success(request, 'we sent you a code', 'success')
-            # if success sent code => new page
             return redirect('accounts:verify_code')
         return redirect('home:home')
 
