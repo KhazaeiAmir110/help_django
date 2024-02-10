15f294b4c0b8692c6f467aa2f0fd75958d21a230
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-10 13:42:10 +0330

    start app products and create models products
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

---
+++ products/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='products/')
+    description = models.TextField()
+    short_description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
