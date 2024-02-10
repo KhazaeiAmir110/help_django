8df2010ea5a2528766dda687b7552a1ca56b36a5
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-10 14:46:22 +0330

    page detail
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.views import View
 from products.models import Product
@@ -8,3 +8,9 @@ class HomeView(View):
     def get(self, request):
         products = Product.objects.filter(available=True)
         return render(request, 'home/home.html', {'products': products})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail-product.html', {'product': product})
