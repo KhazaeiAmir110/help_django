2a72614887f3e269413d98f24012bac9ff7ce8c1
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-11 11:21:50 +0330

    sum price
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- orders/cart.py
+++ orders/cart.py
@@ -32,3 +32,6 @@ class Cart:
         for item in cart.values():
             item['total_price'] = int(item['price']) * item['quantity']
             yield item
+
+    def get_total_price(self):
+        return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
