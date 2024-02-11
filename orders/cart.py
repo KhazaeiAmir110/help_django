c076ef3a2bc2406e2438ef4feecb9b5054ba08a6
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-11 11:16:05 +0330

    cart detail
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- orders/cart.py
+++ orders/cart.py
@@ -1,3 +1,5 @@
+from products.models import Product
+
 CART_SESSION_ID = 'cart'
 
 
@@ -18,3 +20,15 @@ class Cart:
 
     def save(self):
         self.session.modified = True
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['total_price'] = int(item['price']) * item['quantity']
+            yield item
