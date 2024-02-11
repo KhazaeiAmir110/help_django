a5260b9fac126ded275a25b82758315557b924db
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-11 11:34:28 +0330

    cart remove
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,5 @@ app_name = 'orders'
 urlpatterns = [
     path('carts/', views.CartView.as_view(), name='carts'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
+    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
 ]
