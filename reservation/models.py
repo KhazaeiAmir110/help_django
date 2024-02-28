09dd4a8e38cfd8b4f2740dd0761cca9fc1b30a73
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-28 15:10:50 +0330

    update models
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- reservation/models.py
+++ reservation/models.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='scategory',
+                                     null=True, blank=True)
+    is_subb = models.BooleanField(default=False)
     name = models.CharField(max_length=50)
     slug = models.SlugField()
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('home:category_filter', args=[self.slug])
 
 
 class Company(models.Model):
@@ -15,7 +25,9 @@ class Company(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, related_name='companies', blank=True)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/%Y/%m/')
 
     def __str__(self):
         return self.name
