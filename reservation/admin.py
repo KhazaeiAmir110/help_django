8bcd2aebb8746af341d9d3752d1408f11920862b
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-28 11:48:28 +0330

    fix bug
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Company, Category, WorkDate, WorkTime
 
-# Register your models here.
+
+@admin.register(Company)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WorkDate)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WorkTime)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
