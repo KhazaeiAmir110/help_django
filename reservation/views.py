cabb17a9edf26fda8839d5b5e1c167a508cd46b7
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-28 15:11:01 +0330

    crate view home
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from django.views import View
+from .models import Company, Category
+
+
+class HomeView(View):
+    def get(self, request, category_slug=None):
+        companies = Company.objects.all()
+        categories = Category.objects.filter(is_subb=False)
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            companies = Company.objects.filter(category=category)
+        return render(request, 'home/home.html', {'companies': companies, 'categories': categories})
