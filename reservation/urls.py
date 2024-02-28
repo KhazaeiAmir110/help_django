4a7d499c33b6d5df93023e10075b859dfee0bc84
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-02-28 19:27:13 +0330

    view page 2
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, WorkDateView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('category/<slug:category_slug>/', HomeView.as_view(), name='category_filter'),
+    path('work_time/<slug:company_slug>/', WorkDateView.as_view(), name='work_time'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render
 from django.views import View
-from .models import Company, Category
+
+from .models import Company, Category, WorkDate
 
 
 class HomeView(View):
@@ -12,3 +12,12 @@ class HomeView(View):
             category = Category.objects.get(slug=category_slug)
             companies = Company.objects.filter(category=category)
         return render(request, 'home/home.html', {'companies': companies, 'categories': categories})
+
+
+# Page 2
+
+class WorkDateView(View):
+    def get(self, request, company_slug):
+        company = Company.objects.get(slug=company_slug)
+        work_dates = WorkDate.objects.filter(company=company)
+        return render(request, 'reserve/work_time.html', {'work_dates': work_dates, 'company': company})
