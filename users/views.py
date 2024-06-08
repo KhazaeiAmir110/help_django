f4302a29fc90caf407ac3a8678a19adf5237bb79
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-06-08 16:42:40 +0330

    create Login View
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
 
-# Create your views here.
+from .models import User
+
+
+class UserLoginView(LoginView):
+    template_name = 'LR/login.html'
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
