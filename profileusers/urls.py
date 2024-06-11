3536315caa5f746e7d612de9274b291888a67b1a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-06-11 06:13:17 +0330

    add view , url
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- profileusers/urls.py
+++ profileusers/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import ProfileUserView
 
+urlpatterns = [
+    path('', ProfileUserView.as_view(), name='profileuser'),
 ]
--- profileusers/views.py
+++ profileusers/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from users.models import User
+
+
+class ProfileUserView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        # context['demands'] = Demand.objects.filter(user=self.request.user)
+        # context['form'] = DemandForm()
+        return context
+
+    # def post(self, request, *args, **kwargs):
+    #     form = DemandForm(request.POST)
+    #     if form.is_valid():
+    #         demand = form.save(commit=False)
+    #         demand.user = request.user
+    #         demand.save()
+    #     return self.get(request, *args, **kwargs)
