05dbdca72a95777127cbfa3fcc7ddc15569bfe76
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-06-11 05:50:02 +0330

    add urls
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- help_django/urls.py
+++ help_django/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('profile/', include('profileusers.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
