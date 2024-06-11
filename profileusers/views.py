254da332ac84871410afc659088995cc2df8a60f
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-06-11 06:53:42 +0330

    update view
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- profileusers/views.py
+++ profileusers/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from users.models import User
+from home.models import Post, Category, File
 
 
 class ProfileUserView(LoginRequiredMixin, TemplateView):
@@ -11,14 +12,22 @@ class ProfileUserView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
-        # context['demands'] = Demand.objects.filter(user=self.request.user)
-        # context['form'] = DemandForm()
+
+        if self.request.user.is_superuser:
+            context['users'] = User.objects.all()
+            context['posts'] = Post.objects.all()
+            context['categories'] = Category.objects.all()
+            context['files'] = File.objects.all()
+        else:
+            context['post'] = Post.objects.filter(user=self.request.user)
+            # context['form'] = DemandForm()
         return context
 
-    # def post(self, request, *args, **kwargs):
-    #     form = DemandForm(request.POST)
-    #     if form.is_valid():
-    #         demand = form.save(commit=False)
-    #         demand.user = request.user
-    #         demand.save()
-    #     return self.get(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        # form = form(request.POST)
+        form = ''
+        if form.is_valid():
+            demand = form.save(commit=False)
+            demand.user = request.user
+            demand.save()
+        return self.get(request, *args, **kwargs)
