14fd09cd12e331a27c3a1f760b649d3774dbf65b
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-15 08:41:24 +0330

    update view and add forms
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import TodoList
+from .models import TodoList, TodoItem
+from .forms import TodoListForm, TodoItemForm
 
 
 @login_required
 def lists(request):
+    form = TodoListForm()
     todo_list = TodoList.objects.filter(user=request.user)
-    return render(request, 'list.html', {'list': todo_list})
+
+    if request.method == 'POST':
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            new_list = TodoList(title=title, user=request.user)
+            new_list.save()
+            form = TodoListForm()
+
+    return render(request, 'list.html', {'list': todo_list, 'form': form})
 
 
 @login_required
 def list_detail(request, id):
+    form = TodoItemForm()
     selected_list = TodoList.objects.get(id=id)
     items = selected_list.items.all()
+
+    if request.method == 'POST':
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            new_item = TodoItem(title=title, description=description, todo_list=selected_list)
+            new_item.save()
+            form = TodoItemForm()
 
     context = {
         'items': items,
         'selected_list': selected_list,
+        'form': form
     }
     return render(request, 'detail.html', context)
