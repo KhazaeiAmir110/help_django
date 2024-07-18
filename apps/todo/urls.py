66ca256ab3d67f6964299d9ebfe2e04a0ddf7393
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-18 07:55:44 +0330

    work urls in axios
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import lists, list_detail, delete_list, delete_item, send
 
 urlpatterns = [
-    path('', lists, name='todo-list'),
+    path('b/', lists, name='todo-list'),
     path('<int:id>/', list_detail, name='detail'),
     path('delete_list/<int:id>/', delete_list, name='delete_list'),
     path('delete_item/<int:id>', delete_item, name='delete_item'),
