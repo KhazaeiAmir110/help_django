0bff1bd92d2e293275144c3585295b3c74ec9383
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-03-02 14:50:36 +0330

    add base
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- help_django/settings/base.py
+++ help_django/settings/base.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
