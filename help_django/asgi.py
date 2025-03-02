12e16594bfba112f601cc178d1c4b6312106449a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-03-02 14:38:15 +0330

    update
    Signed-off-by: KhazaeiAmir110 <khazaei.amir110@gmail.com>

--- help_django/asgi.py
+++ help_django/asgi.py
@@ -13,20 +13,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
-from chat import consumers
+from chat.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'help_django.settings')
 
 # application = get_asgi_application()
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                [
-                    path('price/', consumers.PriceConsumer.as_asgi())
-                ]
-            )
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        path('ws/chat/', ChatConsumer.as_asgi()),
+    ])
+})
